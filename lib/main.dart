bbd9381c597808d5103cdc8d1cf3633eb934a63c
hamed-deriv <57184669+hamed-deriv@users.noreply.github.com>
2022-04-15 12:47:33 +0800

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_firebase_playground/home_page.dart';
+
+void main() => runApp(const App());
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Firebase Playground',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const HomePage(title: 'Flutter Firebase Playground'),
+    );
+  }
+}
